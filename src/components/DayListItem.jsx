5e2dde6631906a620ac1103e35795a3233f8ef7f
Dylan Wang <wyc6990906@hotmail.com>
2022-07-12 14:50:44 -0600

    DayListItem finish

---
+++ src/components/DayListItem.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import classNames from "classnames";
+import "./DayListItem.scss"
+
+const DayListItem = (props) => {
+  const {spots, selected, name, setDay} = props
+  const dayClass = classNames('day-list__item', {
+    'day-list__item--selected': selected,
+    'day-list__item--full': spots === 0
+  })
+
+  const formatSpots = () => {
+    if (spots === 1) {
+      return "1 spot remaining"
+    }
+    if (spots === 0) {
+      return "no spots remaining"
+    }
+    if (spots > 1) {
+      return `${spots} spots remaining`;
+    }
+
+  }
+
+  return (
+    <li className={dayClass} onClick={() => {
+      setDay(name)
+    }}>
+      <h2 className="text--regular">{name}</h2>
+      <h3 className="text--light">{formatSpots()}</h3>
+    </li>
+  );
+};
+
+export default DayListItem;
