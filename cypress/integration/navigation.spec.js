2ea92d28032b01b609f0d0ac495eb6d54b0264fb
Dylan Wang <wyc6990906@hotmail.com>
2022-07-20 17:17:27 -0600

    add Cypress test

---
+++ cypress/integration/navigation.spec.js
@@ -0,0 +1,54 @@
+describe("appointments", ()=>{
+  beforeEach(()=>{
+    // eslint-disable-next-line no-undef
+    cy.request('GET', '/api/debug/reset')
+
+    // Visits the root of our web server
+    cy.visit("/");
+
+    cy.contains("[data-testid=day]", "Monday"); //default assertion confirms that Monday is in the document
+  })
+  it("should book an interview", ()=>{
+
+
+    // Clicks on the "Add" button in the second appointment
+    cy.get('[alt=Add]').first().click();
+    // Enters their name
+    cy.get('[data-testid=student-name-input]').type("Nicholas Meisenheimer");
+    // Chooses an interviewer
+    cy.get("[alt='Sylvia Palmer']").click();
+    // Clicks the save button
+    cy.contains('Save').click();
+    // Sees the booked appointment
+    cy.contains(".appointment__card--show", "Nicholas Meisenheimer");
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
+
+  })
+  it("should edit an interview", ()=>{
+    // Clicks the edit button for the existing appointment
+    cy.get('[alt=Edit]').first().click({force: true});
+    //Changes the name
+    cy.get('[data-testid=student-name-input]').clear().type("Nicholas Meisenheimer");
+    // Chooses an interviewer
+    cy.get("[alt='Tori Malcolm']").click();
+    // Clicks the save button
+    cy.contains('Save').click();
+    // Sees the booked appointment
+    cy.contains(".appointment__card--show", "Nicholas Meisenheimer");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
+
+  })
+
+  it.only("should cancel an interview", ()=>{
+
+    // Clicks the delete button for the existing appointment
+    cy.get('[alt=Delete]').first().click({force: true});
+    // Clicks the confirm button
+    cy.contains('Confirm').click();
+    cy.contains('Deleting').should('exist')
+    // Sees that the appointment slot is empty
+    cy.contains('Deleting').should('not.exist')
+    cy.contains('.appointment__card--show', 'Archie Cohen').should('not.exist');
+
+  })
+})
