e4fd206ada8b129b271d38ae9bd8f4bca424a166
Dylan Wang <wyc6990906@hotmail.com>
2022-07-19 20:39:59 -0600

    add Form input validate

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -1,14 +1,26 @@
-import React, {useState} from 'react';
-import InterviewerList from "../InterviewerList";
-import Button from "../Button";
+import React, {useState} from "react";
+import InterviewerList from "components/InterviewerList";
+import Button from "components/Button";
 
-const Form = (props) => {
+export default function Form(props) {
   const {student, interviewer, interviewers, onSave, onCancel} = props;
 
   const [studentName, setStudent] = useState(student || "");
   const [interviewerState, setInterviewer] = useState(interviewer || null);
   const [err, setErr] = useState(null);
 
+  const validateInput = function (interviewer, student) {
+    if (!student) {
+      setErr("Student name cannot be blank");
+      return;
+    } else if (!interviewer) {
+      setErr("You must pick an interviewer");
+      return;
+    } else {
+      setErr(null);
+      onSave(studentName, interviewer);
+    }
+  };
 
   const reset = function () {
     setStudent("");
@@ -20,47 +32,45 @@ const Form = (props) => {
     reset();
     onCancel();
   };
-  const validateInput = function (interviewer, student) {
-    if (!student) {
-      setErr("Student name cannot be blank");
-      return;
-    } else if (!interviewer) {
-      setErr("You must pick an interviewer");
-      return;
-    } else {
-      setErr(null);
-      onSave(student, interviewer);
-    }
-  };
 
   return (
-    <main className="appointment__card appointment__card--create">
-      <section className="appointment__card-left">
-        <form autoComplete="off" onSubmit={e => e.preventDefault()}>
+    <main className='appointment__card appointment__card--create'>
+      <section className='appointment__card-left'>
+        <form autoComplete='off' onSubmit={e => e.preventDefault()}>
           <input
+            className='appointment__create-input text--semi-bold'
+            name='name'
+            type='text'
+            placeholder='Enter Student Name'
             value={studentName}
-            onChange={(e) => {
-              setStudent(e.target.value)
+            onChange={e => {
+              setStudent(e.target.value);
               setErr(null);
             }}
             data-testid={"student-name-input"}
-            className="appointment__create-input text--semi-bold"
-            name="name"
-            type="text"
-            placeholder="Enter Student Name"
           />
         </form>
-        <InterviewerList interviewers={interviewers} value={interviewerState} onChange={setInterviewer}
+        <section className='appointment__validation'>{err}</section>
+        <InterviewerList
+          interviewers={interviewers}
+          interviewer={interviewerState}
+          setInterviewer={setInterviewer}
         />
       </section>
-      <section className="appointment__card-right">
-        <section className="appointment__actions">
-          <Button onClick={cancel} danger>Cancel</Button>
-          <Button onClick={() => validateInput(interviewerState, studentName)} confirm>Save</Button>
+
+      <section className='appointment__card-right'>
+        <section className='appointment__actions'>
+          <Button danger onClick={cancel}>
+            Cancel
+          </Button>
+          <Button
+            confirm
+            onClick={() => validateInput(interviewerState, studentName)}
+          >
+            Save
+          </Button>
         </section>
       </section>
     </main>
   );
-};
-
-export default Form;
+}
--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -3,9 +3,20 @@ import React from "react";
 import { render, cleanup } from "@testing-library/react";
 
 import Application from "components/Application";
+import Appointment from "../Appointment";
 
 afterEach(cleanup);
 
-it("renders without crashing", () => {
-  render(<Application />);
+describe("Appointment", () => {
+  it("renders without crashing", () => {
+    render(<Appointment />);
+  });
+
+  it("does something it is supposed to do", () => {
+    // ...
+  });
+
+  it("does something else it is supposed to do", () => {
+    // ...
+  });
 });
